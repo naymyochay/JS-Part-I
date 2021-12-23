d5220e5e62b10d0e2202cab68a7b439d08105e82
naymyochay <naymyoaung.atechnician@gmail.com>
2021-12-23 13:57:00 +0630

    Coding_Challenge_One_and_Two

---
+++ 1_Coding_Challenge.js
@@ -0,0 +1,18 @@
+/**
+* Coding Challenge Part - I
+* Coding Challenge Part - II
+* Challenged Lesson - 1
+*/
+
+//For Mark
+const WM = 78;
+const HM = 1.69;
+const BMI_M = 78 / (1.69 * 1.69);
+
+// For John
+const WJ = 92;
+const HJ = 1.95;
+const BMI_J = 92 / (1.95 * 1.95);
+
+// For Result comparing with BMI of Mark and BMI of John
+console.log("BMI of Mark = "+ BMI_M.toFixed(3) +" > BMI of John = "+BMI_J.toFixed(3));
